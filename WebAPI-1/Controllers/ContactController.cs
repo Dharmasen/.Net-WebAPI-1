a3f646f49fb6b1b35866101a25646dcb28481127
Dharmasen Yadav PC <Dharmasen.pclenovo@outlook.com>
2022-02-09 01:33:44 +0530

    Added models.

---
+++ WebAPI-1/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+
+namespace WebAPI_1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        // GET: api/<ValuesController> // api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        // GET api/<ValuesController>/5 
+        [HttpGet("{id}")]      //https://localhost:44319/api/values/6 
+        public string Get(int id)
+        {
+            return "value: " + id.ToString();
+        }
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
---
+++ WebAPI-1/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+
+public class Contact
+{
+    public int id { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public bool IsDeleted { get; set; } = false;
+
+
+}
+
